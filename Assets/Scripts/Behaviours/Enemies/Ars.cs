6a9c75b4ea55dc444e4771174f4400a6ed3f3908
Daimnon <49341329+Daimnon@users.noreply.github.com>
2023-06-10 20:04:37 +0300

    Ars Animations bad

--- Assets/Scripts/Behaviours/Enemies/Ars.cs
+++ Assets/Scripts/Behaviours/Enemies/Ars.cs
@@ -7,9 +7,12 @@ using UnityEngine;
 public class Ars : Enemy
 {
     [SerializeField] private float _chaseSpeed, _chaseDistance, _stopDistance, _yOffset, _attackSpeed;
+    [SerializeField] private Animator _animator;
     
     private PlayerController _playerController;
     private IEnumerator _attackRoutine;
+    private float _moveSpeed = 0;
+    private bool _isAwake;
 
     private void Awake()
     {
@@ -35,8 +38,17 @@ public class Ars : Enemy
 
     protected override void PlayerInsight() //chase after player
     {
+        if (!_isAwake)
+        {
+            _animator.SetBool("IsAwoken", true);
+            _isAwake = true;
+        }
+
         if (IsInteracting)
+        {
+            _animator.SetBool("IsPunching", false);
             IsInteracting = false;
+        }
 
         if (transform.position.x < Target.transform.position.x)
             Renderer.flipX = true;
@@ -46,17 +58,27 @@ public class Ars : Enemy
         _playerController = Target.GetComponent<PlayerController>();
 
         Vector3 offset = new(0.0f, _yOffset, 0.0f);
+
+        Vector2 lastMovePos = new(transform.position.x, transform.position.y);
         transform.position = Vector2.MoveTowards(transform.position, Target.transform.position + offset, _chaseSpeed * Time.deltaTime);
+
+        _moveSpeed = 1;
+        _animator.SetFloat("Speed", _moveSpeed);
     }
     protected override void PlayerNotInsight() //patrol
     {
         if (IsInteracting)
+        {
             IsInteracting = false;
+            _moveSpeed = 0;
+            _animator.SetFloat("Speed", _moveSpeed);
+        }
     }
     protected override void Interacting()
     {
         if (!IsInteracting)
         {
+            _animator.SetBool("IsPunching", true);
             StartCoroutine(_attackRoutine);
             IsInteracting = true;
         }
